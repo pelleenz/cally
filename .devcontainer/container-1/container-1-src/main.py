7ebb5e2a4e08064192b065f337d8f9645982d80c
Pelle Enz <pelle-enz@t-online.de>
2024-04-20 20:28:39 +0200

    v0.7 polishing

--- .devcontainer/container-1/container-1-src/main.py
+++ .devcontainer/container-1/container-1-src/main.py
@@ -1,45 +1,35 @@
 import caldav
 import time
-from icalendar import Event
 from datetime import datetime
 from icalevents.icalevents import events as ical_event
-import configparser
+import os
 import logging
 import sys
 
 def main():
-  logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-  logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
-  config = config_parser({})
+  logging.basicConfig(
+    stream=sys.stdout,
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+  logging.basicConfig(
+    stream=sys.stderr,
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.ERROR,
+    datefmt='%Y-%m-%d %H:%M:%S')
+  
+  ical_file_path = os.environ['ICS_PATH']
+  caldav_url = os.environ['CALDAV_URL']
+  username = os.environ['CALDAV_USER']
+  password = os.environ['CALDAV_PASS']
+  target_cal = os.environ['CALDAV_CALENDAR']
+  prefix = os.environ['CALDAV_PREFIX']
   while True:
-    ical_events = ical_loader(config["ical_file_path"])
-    client = caldav_conn(config["caldav_url"], config["username"], config["password"])
-    calendar = cal_finder(client, config)
-    caldav_parser(ical_events, calendar, config)
+    ical_events = ical_loader(ical_file_path)
+    client = caldav_conn(caldav_url, username, password)
+    calendar = cal_finder(client, target_cal)
+    caldav_parser(ical_events, calendar, prefix)
     time.sleep(60)
-
-def config_parser(args, path = "./"):
-  try:
-      parse = configparser.RawConfigParser()
-      if path[-4:] == ".ini":
-        parse.read(path)
-      elif path[-1] == "/":
-        parse.read(path + "cally.ini")
-      else:
-        parse.read(path + "/cally.ini")
-  except:
-    e = "Config file could not be read"
-    logging.error(e)
-    raise(e)
-      
-  args['ical_file_path'] = parse.get("Main", 'ical_file_path')
-  args['caldav_url'] = parse.get("Main", 'caldav_url')
-  args['username'] = parse.get("Main", 'username')
-  args['password'] = parse.get("Main", 'password')
-  args['target_cal'] = parse.get("Main", 'target_cal')
-  args['prefix'] = parse.get("Main", 'prefix')
-    
-  return(args)
 
 def caldav_conn(caldav_url, username, password):
   client = caldav.DAVClient(url=caldav_url, username=username, password=password)
@@ -50,9 +40,9 @@ def ical_loader(ical_file_path):
   ical_events = ical_event(ical_file_path)
   return ical_events
 
-def caldav_parser(ical_events, calendar, config):
+def caldav_parser(ical_events, calendar, prefix):
   for event in ical_events:
-        event_summary = str(config["prefix"] + " " + event.summary)
+        event_summary = str(prefix + " " + event.summary)
         event_start = event.start
         event_end = event.end
         
@@ -64,11 +54,11 @@ def caldav_parser(ical_events, calendar, config):
         
         logging.info(f"Duplicate found, Event {event_summary} not added")
         
-def cal_finder(client, config):
+def cal_finder(client, target_cal):
   principal = client.principal()
   calendars = principal.calendars()
   for calendar in calendars:
-    if str(calendar) == config["target_cal"]:
+    if str(calendar) == target_cal:
       return calendar
     
   logging.error(f"Calendar {str(calendar)} not found")
@@ -89,16 +79,14 @@ def duplicate_check (calendar, event_summary, event_start, event_end):
     if (event_summary == res.get('SUMMARY')) and \
            (convert_datetime_format(event_start) == res.get('DTSTART;TZID=CEST;VALUE=DATE-TIME')) and \
            (convert_datetime_format(event_end) == res.get('DTEND;TZID=CEST;VALUE=DATE-TIME')):
-            return True  # Ein Duplikat wurde gefunden
+            return True
   
-  return False  # Kein Duplikat gefunden
+  return False
   
 def convert_datetime_format(input_datetime_str):
-    # Parse the input datetime string
     input_format = "%Y-%m-%d %H:%M:%S%z"
     dt = datetime.strptime(str(input_datetime_str), input_format)
 
-    # Convert the datetime to a different format
     output_format = "%Y%m%dT%H%M%S"
     output_datetime_str = dt.strftime(output_format)
 
