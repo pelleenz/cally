29cf870e030251929bc6dc66a554eaf0580d32ae
Pelle Enz <pelle-enz@t-online.de>
2024-04-21 09:25:52 +0200

    backend finished

--- .devcontainer/container-1/container-1-src/main.py
+++ .devcontainer/container-1/container-1-src/main.py
@@ -42,17 +42,18 @@ def ical_loader(ical_file_path):
 
 def caldav_parser(ical_events, calendar, prefix):
   for event in ical_events:
-        event_summary = str(prefix + " " + event.summary)
-        event_start = event.start
-        event_end = event.end
-        
-        duplicate = duplicate_check(calendar, event_summary, event_start, event_end)
-        
-        if duplicate is False:        
-          calendar.add_event(summary=event_summary, dtstart=event_start, dtend=event_end)
-          logging.info(f"Event {event_summary} added to Calendar")
-        
-        logging.info(f"Duplicate found, Event {event_summary} not added")
+    event_summary = str(prefix + " " + event.summary)
+    event_start = event.start
+    event_end = event.end
+    event_uid = event.uid
+  
+    duplicate = duplicate_check(calendar, event_summary, event_start, event_end, event_uid)
+    
+    if duplicate is False:        
+      calendar.add_event(summary=event_summary, dtstart=event_start, dtend=event_end, uid=event_uid)
+      logging.info(f"Event {event_summary} added to Calendar")
+    else:
+      logging.info(f"Duplicate found, Event {event_summary} not added")
         
 def cal_finder(client, target_cal):
   principal = client.principal()
@@ -64,8 +65,11 @@ def cal_finder(client, target_cal):
   logging.error(f"Calendar {str(calendar)} not found")
   raise Exception("Calender not found on the server.")
 
-def duplicate_check (calendar, event_summary, event_start, event_end):
+def duplicate_check(calendar, event_summary, event_start, event_end, event_uid):
   events_in_range = calendar.events()
+  
+  if len(events_in_range) == 0:
+    return False
   
   for event in events_in_range:
     event_data = event.data
@@ -75,13 +79,11 @@ def duplicate_check (calendar, event_summary, event_start, event_end):
       if ':' in sub: 
         res.append(map(str.strip, sub.split(':', 1))) 
     res = dict(res)
-   
-    if (event_summary == res.get('SUMMARY')) and \
-           (convert_datetime_format(event_start) == res.get('DTSTART;TZID=CEST;VALUE=DATE-TIME')) and \
-           (convert_datetime_format(event_end) == res.get('DTEND;TZID=CEST;VALUE=DATE-TIME')):
-            return True
-  
-  return False
+    print(res.get('UID'))
+    
+    if (event_uid == res.get('UID')):
+      return True
+    return False
   
 def convert_datetime_format(input_datetime_str):
     input_format = "%Y-%m-%d %H:%M:%S%z"
